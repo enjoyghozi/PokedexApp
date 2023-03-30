f830a896bda22e0f046a1f805c59fcae97619581
Ghosi Adam <adamghosi03@gmail.com>
2023-03-30 23:08:06 +0700

    merapikan code

--- lib/Screens/login_screen.dart
+++ lib/Screens/login_screen.dart
@@ -20,147 +20,204 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Center(
-          child: Container(
-            height: 700,
-            width: 350,
+    return SafeArea(
+      child: Scaffold(
+        // resizeToAvoidBottomInset: false,
+        body: Container(
+          margin: EdgeInsets.all(24),
+          child: SingleChildScrollView(
             child: Column(
               children: [
-                Image.asset('images/pokemon_logo.png'),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text("Let's get started", 
-                  style: TextStyle(
-                    fontSize: 33, 
-                    fontWeight: FontWeight.bold, 
-                    color: Colors.amber),),
-                ),
-                Text("There's No Sense In Going Out Of ", 
-                  textAlign: TextAlign.center, 
-                  style: TextStyle(
-                    fontSize: 17, 
-                    fontWeight: FontWeight.bold, 
-                    color: Color.fromARGB(227, 161, 161, 158),
-                    )),
-                Text("Your Way To Get Somebody To Like You", 
-                  textAlign: TextAlign.center, 
-                  style: TextStyle(
-                    fontSize: 17, 
-                    fontWeight: FontWeight.bold, 
-                    color: Color.fromARGB(227, 161, 161, 158),
-                    )),
-                SizedBox(height: 40),
-                Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text("Name",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Colors.blue
-                      ),
-                    ),
-                  ),
-                CustomTextFormField(
-                  controller: _nameController, 
-                  hintText: 'Enter your name', 
-                  obscureText: false, 
-                  icon: Icons.person,
-                  validator: (value){
-                    if(value == ''){
-                      return "Please enter your name";
-                    }else {
-                      return null;
-                    }
-                  },
-                ),
-                SizedBox(height: 40),
-                Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text("Password",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Colors.blue
-                      ),
-                    ),
-                  ),
-                TextFormField(
-                  controller: _passwordController, 
-                  decoration: InputDecoration(
-                    hintText: 'Enter your password', 
-                    prefixIcon: Icon(Icons.lock),
-                    prefixIconColor: Colors.blue,
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                        color: Colors.blue,
-                        width: 1.0,
-                      ),
-                    ),
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _obscureText = !_obscureText;
-                        });
-                      },
-                      child: Icon(_obscureText 
-                      ? Icons.visibility 
-                      : Icons.visibility_off),
-                    )
-                  ),
-                  obscureText: _obscureText, 
-                  validator: (value){
-                  if(value == ''){
-                    return "Please enter your password";
-                  }else {
-                    return null;
-                  }
-                },
-                ),
-                SizedBox(height: 55),
-                InkWell(
-                  child: Container(
-                    height: 55,
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    child: Center(
-                      child: Text('Sign In',
-                          style: TextStyle( 
-                              color: Colors.white, 
-                              fontSize: 18, 
-                              fontWeight: FontWeight.bold
-                            ))
-                          ),
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(50)
-                    ),
-                  ),
-                  onTap: () {
-                    print('sign in pressed');
-                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext) => HomeScreen()));
-                  }
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Don't have an account?"),
-                    TextButton(onPressed: () {
-                      print('Sign up button');
-                      Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext) => SignUpScreen()));
-                    }, child: Text("Sign Up", 
-                        style: TextStyle(
-                          color: Colors.amber,
-                          fontWeight: FontWeight.bold, 
-                        ), 
-                      )
-                    )
-                  ],
-                ),
+                _logo(context),
+                _header(context),
+                _description(context),
+                _inputField(context),
+                _buttonSignIn(context),
+                _row(context)
               ],
             ),
           ),
-        ),
+        )
       ),
+    );
+  }
+  _logo(context) {
+    return Container(
+      height: 200,
+      width: 350,
+      child: Column(
+        children: [
+          SizedBox(height: 50),
+          Image.asset('images/pokemon_logo.png'),
+        ],
+      ),
+    );
+  }
+
+  _header(context) {
+    return Column(
+      children: [
+        Center(
+          child: Text("Let's get started. ", 
+              textAlign: TextAlign.center,
+              style: TextStyle(
+              fontSize: 33, 
+              fontWeight: FontWeight.bold, 
+              color: Colors.amber,
+              ),
+          ),
+        ),
+      ],
+    );
+  }
+  _description(context) {
+    return Column(
+      children: [
+        SizedBox(height: 20),
+        Center(
+          child: Text("There's No Sense In Going Out Of", 
+              textAlign: TextAlign.center,
+              style: TextStyle(
+              fontSize: 17, 
+              fontWeight: FontWeight.bold, 
+              color: Colors.grey,
+              ),
+          ),
+        ),
+        Center(
+          child: Text("Your Way To Get Somebody To Like You.", 
+              textAlign: TextAlign.center,
+              style: TextStyle(
+              fontSize: 17, 
+              fontWeight: FontWeight.bold, 
+              color: Colors.grey,
+              ),
+          ),
+        ),
+      ],
+    );
+  }
+  _inputField(context) {
+    return Column(
+      children: [
+        SizedBox(height: 50),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: Text("Name",
+            textAlign: TextAlign.start,
+            style: TextStyle(
+              color: Colors.blue
+            ),
+          ),
+        ),
+        CustomTextFormField(
+          controller: _nameController, 
+          hintText: 'Enter your name', 
+          obscureText: false, 
+          icon: Icons.person,
+          validator: (value){
+            if(value == ''){
+              return "Please enter your name";
+            }else {
+              return null;
+            }
+          },
+        ),
+        SizedBox(height: 20),
+        Align(
+            alignment: Alignment.bottomLeft,
+            child: Text("Password",
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                color: Colors.blue
+              ),
+            ),
+          ),
+        TextFormField(
+          controller: _passwordController, 
+          decoration: InputDecoration(
+            hintText: 'Enter your password', 
+            prefixIcon: Icon(Icons.lock),
+            prefixIconColor: Colors.blue,
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                color: Colors.blue,
+                width: 1.0,
+              ),
+            ),
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _obscureText = !_obscureText;
+                });
+              },
+              child: Icon(_obscureText 
+              ? Icons.visibility 
+              : Icons.visibility_off),
+            )
+          ),
+          obscureText: _obscureText, 
+          validator: (value){
+            if(value == ''){
+              return "Please enter your password";
+            }else {
+              return null;
+            }
+          },
+        ),
+      ],
+    );
+  }
+
+  _buttonSignIn(context) {
+    return Column(
+      children: [
+        SizedBox(height: 80),
+        InkWell(
+          child: Container(
+            height: 55,
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: Center(
+              child: Text('Sign In',
+                  style: TextStyle( 
+                      color: Colors.white, 
+                      fontSize: 18, 
+                      fontWeight: FontWeight.bold
+                    ))
+                  ),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(50)
+            ),
+          ),
+          onTap: () {
+            print('sign in pressed');
+            Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext) => HomeScreen()));
+          }
+        ),
+      ],
+    );
+  }
+  _row(context) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Don't have an account?"),
+            TextButton(onPressed: () {
+              print('Sign up button');
+              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext) => SignUpScreen()));
+            }, child: Text("Sign Up", 
+                style: TextStyle(
+                  color: Colors.amber,
+                  fontWeight: FontWeight.bold, 
+                ), 
+              )
+            )
+          ],
+        ),
+      ],
     );
   }
 }
--- lib/Screens/signup_screen.dart
+++ lib/Screens/signup_screen.dart
@@ -21,206 +21,244 @@ bool _obscureText = true;
 class _SignUpScreenState extends State<SignUpScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          margin: EdgeInsets.all(24),
+          child: SingleChildScrollView(
             child: Column(
               children: [
-                Align(
-                  alignment: Alignment.topLeft,
-                  child: InkWell(
-                      child: Container(
-                        height: 45,
-                        width: 45,
-                        decoration: BoxDecoration(
-                          color: Colors.amber,
-                          borderRadius: BorderRadius.circular(10)
-                        ),
-                        child: Icon(
-                          Icons.arrow_back,
-                          color: Colors.white,
-                          size: 20.0,
-                        ),
-                      ),
-                    onTap: () {
-                      print('back to LogIn');
-                      Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext) => LoginScreen()));
-                    }
-                  ),
-                ),
-                CircleAvatar(
-                  radius: 60,
-                  backgroundImage: AssetImage('images/Pokemon_register.jpg'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Text("Register", 
-                  style: TextStyle(
-                    fontSize: 33, 
-                    fontWeight: FontWeight.bold, 
-                    color: Color.fromARGB(228, 255, 218, 7)),),
-                ),
-                SizedBox(height: 30),
-                Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text("Name",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Colors.blue
-                      ),
-                    ),
-                  ),
-                CustomTextFormField(
-                  controller: _nameController, 
-                  hintText: 'Enter your name', 
-                  obscureText: false, 
-                  icon: Icons.person,
-                  validator: (value){
-                    if(value == ''){
-                      return "Please enter your name";
-                    }else {
-                      return null;
-                    }
-                  },
-                ),
-                SizedBox(height: 20),
-                Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text("Email",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Colors.blue
-                      ),
-                    ),
-                  ),
-                CustomTextFormField(
-                  controller: _emailController, 
-                  hintText: 'Enter your email', 
-                  obscureText: false, 
-                  icon: Icons.email,
-                  validator: (value){
-                    if(value == ''){
-                      return "Please enter your email";
-                    }else {
-                      return null;
-                    }
-                  },
-                ),
-                SizedBox(height: 20),
-                 Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text("Password",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Colors.blue
-                      ),
-                    ),
-                  ),
-                TextFormField(
-                  controller: _passwordController, 
-                  decoration: InputDecoration(
-                    hintText: 'Enter your password', 
-                    prefixIcon: Icon(Icons.lock),
-                    prefixIconColor: Colors.blue,
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                        color: Colors.blue,
-                        width: 1.0,
-                      ),
-                    ),
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _obscureText = !_obscureText;
-                        });
-                      },
-                      child: Icon(_obscureText 
-                      ? Icons.visibility 
-                      : Icons.visibility_off),
-                    )
-                  ),
-                  obscureText: _obscureText, 
-                  validator: (value){
-                  if(value == ''){
-                    return "Please enter your password";
-                  }else {
-                    return null;
-                  }
-                },
-                ),
-                SizedBox(height: 20),
-                Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Text("Re-Password",
-                      textAlign: TextAlign.start,
-                      style: TextStyle(
-                        color: Colors.blue
-                      ),
-                    ),
-                  ),
-                TextFormField(
-                  controller: _rePasswordController, 
-                  decoration: InputDecoration(
-                    hintText: 'Re-enter your password', 
-                    prefixIcon: Icon(Icons.lock),
-                    prefixIconColor: Colors.blue,
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                        color: Colors.blue,
-                        width: 1.0,
-                      ),
-                    ),
-                    suffixIcon: GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          _obscureText = !_obscureText;
-                        });
-                      },
-                      child: Icon(_obscureText 
-                      ? Icons.visibility 
-                      : Icons.visibility_off),
-                    )
-                  ),
-                  obscureText: _obscureText, 
-                  validator: (value){
-                  if(value == ''){
-                    return "Please re-enter your password";
-                  }else {
-                    return null;
-                  }
-                },
-                ),
-                SizedBox(height: 30),
-                InkWell(
-                  child: Container(
-                    height: 55,
-                    width: MediaQuery.of(context).size.width * 0.9,
-                    child: Center(
-                      child: Text('Register',
-                          style: TextStyle( 
-                              color: Colors.white, 
-                              fontSize: 18, 
-                              fontWeight: FontWeight.bold
-                            ))
-                          ),
-                    decoration: BoxDecoration(
-                      color: Colors.amber,
-                      borderRadius: BorderRadius.circular(50)
-                    ),
-                  ),
-                  onTap: () {
-                    print('sign in pressed');
-                    Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext) => HomeScreen()));
-                  }
-                ),
+                _backButton(context),
+                _circleAvatar(context),
+                _header(context),
+                _inputField(context),
+                _buttonRegister(context)
               ],
             ),
           ),
-        ),
+        )
       ),
+    );
+  }
+
+  _backButton(context) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: InkWell(
+              child: Container(
+                height: 45,
+                width: 45,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Icon(
+                  Icons.arrow_back,
+                  color: Colors.white,
+                  size: 20.0,
+                ),
+              ),
+            onTap: () {
+              print('back to LogIn');
+              Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext) => LoginScreen()));
+            }
+          ),
+        ),
+      ],
+    );
+  }
+
+  _circleAvatar(context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 60,
+          backgroundImage: AssetImage('images/Pokemon_register.jpg'),
+        ),
+      ],
+    );
+  }
+
+  _header(context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Text("Register", 
+          style: TextStyle(
+            fontSize: 33, 
+            fontWeight: FontWeight.bold, 
+            color: Color.fromARGB(228, 255, 218, 7)),),
+        ),
+      ],
+    );
+  }
+
+  _inputField(context) {
+    return Column(
+      children: [
+        Align(
+            alignment: Alignment.bottomLeft,
+            child: Text("Name",
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                color: Colors.blue
+              ),
+            ),
+          ),
+        CustomTextFormField(
+          controller: _nameController, 
+          hintText: 'Enter your name', 
+          obscureText: false, 
+          icon: Icons.person,
+          validator: (value){
+            if(value == ''){
+              return "Please enter your name";
+            }else {
+              return null;
+            }
+          },
+        ),
+        SizedBox(height: 20),
+        Align(
+            alignment: Alignment.bottomLeft,
+            child: Text("Email",
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                color: Colors.blue
+              ),
+            ),
+          ),
+        CustomTextFormField(
+          controller: _emailController, 
+          hintText: 'Enter your email', 
+          obscureText: false, 
+          icon: Icons.email,
+          validator: (value){
+            if(value == ''){
+              return "Please enter your email";
+            }else {
+              return null;
+            }
+          },
+        ),
+        SizedBox(height: 20),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Text("Password",
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                color: Colors.blue
+              ),
+            ),
+          ),
+        TextFormField(
+          controller: _passwordController, 
+          decoration: InputDecoration(
+            hintText: 'Enter your password', 
+            prefixIcon: Icon(Icons.lock),
+            prefixIconColor: Colors.blue,
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                color: Colors.blue,
+                width: 1.0,
+              ),
+            ),
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _obscureText = !_obscureText;
+                });
+              },
+              child: Icon(_obscureText 
+              ? Icons.visibility 
+              : Icons.visibility_off),
+            )
+          ),
+          obscureText: _obscureText, 
+          validator: (value){
+          if(value == ''){
+            return "Please enter your password";
+          }else {
+            return null;
+          }
+        },
+        ),
+        SizedBox(height: 20),
+        Align(
+            alignment: Alignment.bottomLeft,
+            child: Text("Re-Password",
+              textAlign: TextAlign.start,
+              style: TextStyle(
+                color: Colors.blue
+              ),
+            ),
+          ),
+        TextFormField(
+          controller: _rePasswordController, 
+          decoration: InputDecoration(
+            hintText: 'Re-enter your password', 
+            prefixIcon: Icon(Icons.lock),
+            prefixIconColor: Colors.blue,
+            enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                color: Colors.blue,
+                width: 1.0,
+              ),
+            ),
+            suffixIcon: GestureDetector(
+              onTap: () {
+                setState(() {
+                  _obscureText = !_obscureText;
+                });
+              },
+              child: Icon(_obscureText 
+              ? Icons.visibility 
+              : Icons.visibility_off),
+            )
+          ),
+          obscureText: _obscureText, 
+          validator: (value){
+            if(value == ''){
+              return "Please re-enter your password";
+            }else {
+              return null;
+            }
+          },
+        ),
+      ],
+    );
+  }
+
+  _buttonRegister(context) {
+    return Column(
+      children: [
+        SizedBox(height: 30),
+        InkWell(
+          child: Container(
+            height: 55,
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: Center(
+              child: Text('Register',
+                  style: TextStyle( 
+                      color: Colors.white, 
+                      fontSize: 18, 
+                      fontWeight: FontWeight.bold
+                    ))
+                  ),
+            decoration: BoxDecoration(
+              color: Colors.amber,
+              borderRadius: BorderRadius.circular(50)
+            ),
+          ),
+          onTap: () {
+            print('sign in pressed');
+            Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext) => HomeScreen()));
+          }
+        ),
+      ],
     );
   }
 }
